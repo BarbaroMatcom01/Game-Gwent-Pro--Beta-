b594eb7b892b759e382baf27c62ad72119b57fcb
Barbaro Yoel <barbaromartinez2003@gmail.com>
2024-04-15 09:50:39 -0400

    Creacion de las Cartas y el Deck

--- Assets/Scripts/DecksDataBase/DeckData.cs
+++ Assets/Scripts/DecksDataBase/DeckData.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Deck", menuName = "Deck")]
 public class DeckData : ScriptableObject
 {
-  /*
-    Define una propiedad pública de tipo List<CardData> llamada DeckCards.
-    Esta propiedad almacenará una lista de objetos CardData, representando las cartas que componen el mazo.
-  */
-  public List<CardData> DeckCards;
-
-  // Esta propiedad almacenará un objeto LeaderCard, representando la carta líder del mazo.
-  public LeaderCard LeaderCard;
-
+    /*
+      Define una propiedad pública de tipo List<CardData> llamada DeckCards.
+      Esta propiedad almacenará una lista de objetos CardData, representando las cartas que componen el mazo.
+    */
+    public List<CardData> DeckCards ;
+    
+    // Esta propiedad almacenará un objeto LeaderCard, representando la carta líder del mazo.
+    public LeaderCard LeaderCard;
+    
 }
